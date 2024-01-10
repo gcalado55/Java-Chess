b5a7e9711ebb286b91232bfd8a62978defac05bd
Gabriel Calado <gcalado501@gmail.com>
2024-01-10 15:01:15 -0300

    Moving Pieces

--- Java Chess/src/Application/Progam.java
+++ Java Chess/src/Application/Progam.java
@@ -3,14 +3,31 @@ package Application;
 import BoardGame.Board;
 import BoardGame.Position;
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Progam {
 
     public static void main(String[] args){
 
-
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        Ui.printBoard(chessMatch.getPieces());
+
+        while (true){
+            Ui.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = Ui.readChessPosition(sc);
+
+            System.out.print("\nTarget: ");
+            ChessPosition target = Ui.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performChessMove(source,target);
+        }
+
+
 
     }
 }
